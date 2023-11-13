4aa85420a54ed0abac42894b94586e878d19b5b4
shqkel <shqkel1863@gmail.com>
2023-11-13 10:33:38 +0900

    Dog#bark 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,10 +1,14 @@
 package com.sh.app;
 
+import com.sh.app.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello Git");
+		
+		new Dog().bark();
 	}
 
 }
---
+++ src/com/sh/app/animal/Dog.java
@@ -0,0 +1,7 @@
+package com.sh.app.animal;
+
+public class Dog {
+	public void bark() {
+		System.out.println("멍멍~");		
+	}
+}
