350fee0531d347207699753329677e5765f45d12
shqkel <shqkel1863@gmail.com>
2023-11-13 12:12:16 +0900

    Cat#jump 관련 기능 취소

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,7 +1,6 @@
 package com.sh.app;
 
 import com.sh.app.animal.Bird;
-import com.sh.app.animal.Cat;
 import com.sh.app.animal.Chicken;
 import com.sh.app.animal.Dog;
 import com.sh.app.animal.Snake;
@@ -15,7 +14,6 @@ public class Main {
 		System.out.println("동물기능 시작~");
 		System.out.println("Animal Go~");
 		new Dog().bark();
-		new Cat().jump();
 		new Snake().crawl();
 		new Bird().fly();
 		new Chicken().run();
--- src/com/sh/app/animal/Cat.java
+++
@@ -1,7 +0,0 @@
-package com.sh.app.animal;
-
-public class Cat {
-	public void jump() {
-		System.out.println("고양이 점프!");
-	}
-}
