4a81b14b2fa1dfe0ef21b50ec12b710b04d7b36d
shqkel <shqkel1863@gmail.com>
2023-11-13 11:16:34 +0900

    Snake#crawl 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -2,6 +2,7 @@ package com.sh.app;
 
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
+import com.sh.app.animal.Snake;
 
 public class Main {
 
@@ -11,6 +12,7 @@ public class Main {
 		
 		new Dog().bark();
 		new Cat().jump();
+		new Snake().crawl();
 	}
 
 }
---
+++ src/com/sh/app/animal/Snake.java
@@ -0,0 +1,8 @@
+package com.sh.app.animal;
+
+public class Snake {
+	
+	public void crawl() {
+		System.out.println("뱀 기어가기~");
+	}
+}
